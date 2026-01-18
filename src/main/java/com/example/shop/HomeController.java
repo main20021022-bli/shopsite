2ad0234b3c6212d3ee338e9152bda82e6bf8df13
main20021022-bli <main20021022@gmail.com>
2026-01-18 22:02:40 +0900

    namae: Rename package from 'demo' to 'shop'

--- src/main/java/com/example/shop/HomeController.java
+++ src/main/java/com/example/shop/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.shop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/example/shop/ShopRepository.java
+++ src/main/java/com/example/shop/ShopRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
